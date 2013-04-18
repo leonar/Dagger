1cd3181229e6f455082037300d1fab7dbd216182
leonar <leonarfox@163.com>
2013-04-18 11:46:44 +0800

    add settings

---
+++ src/com/netease/checkuistyle/Settings.java
@@ -0,0 +1,36 @@
+package com.netease.checkuistyle;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Settings {
+	public static Properties prop = getProperties();
+
+	public static int ScreenShotType = Integer.parseInt(prop.getProperty(
+			"ScreenShotType", "1"));
+
+	public static String SampleImagePath = prop.getProperty("SampleImagePath",
+			"res/samples/");
+
+	public static String ContrastImagePath = prop.getProperty(
+			"ContrastImagePath", "images/");
+
+	public static double MaxColorThreshold = Double.parseDouble(prop
+			.getProperty("MaxColorThreshold", "0"));
+
+	public static String getProperty(String Property) {
+		return prop.getProperty(Property);
+	}
+
+	public static Properties getProperties() {
+		Properties prop = new Properties();
+		try {
+			FileInputStream file = new FileInputStream("type.properties");
+			prop.load(file);
+			file.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
+}
